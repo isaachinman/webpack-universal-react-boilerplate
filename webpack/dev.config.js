168c4e1f0cedacf91808afc2c2736eb2ed530256
isaachinman <isaacshinman@gmail.com>
2016-07-22 14:00:16 +0200

    Correcting dir name

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -4,7 +4,7 @@ require('babel-polyfill');
 var fs = require('fs');
 var path = require('path');
 var webpack = require('webpack');
-var assetsPath = path.resolve(__dirname, '../static/dist');
+var assetsPath = path.resolve(__dirname, '../build/dist');
 var host = (process.env.HOST || 'localhost');
 var port = (+process.env.PORT + 1) || 3001;
 
